273e3b478cface015bdd20a6f359aeca7fba536b
Hasan <cs44ui@gmail.com>
2024-09-01 19:41:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar"
+import NewsBoard from "./Components/NewsBoard"
+import NewsItem from "./Components/NewsItem"
+ 
+ const App = () => {
+   return (
+     <div>
+    <Navbar />
+    <NewsBoard/>
+    <NewsItem/>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
